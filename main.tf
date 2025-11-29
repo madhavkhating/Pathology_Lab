532f74a7eaf39322e68549d64b9a7c5b0236e063
Madhav Khating <mkhating9@gmail.com>
2025-11-29 13:47:30 +0530

    second commit, adding main tf file and config tf file

---
+++ main.tf
@@ -0,0 +1,32 @@
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "pathology-lab-vpc"
+  }
+}
+
+# Private Subnet 1 (ap-south-1a)
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "pathology-lab-private-subnet-1a"
+  }
+}
+
+# Private Subnet 2 (ap-south-1b)
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "pathology-lab-private-subnet-1b"
+  }
+}
