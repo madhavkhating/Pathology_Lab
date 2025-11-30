c0abdc14361ad60353791c5aac50af9e07e52f5f
Madhav Khating <mkhating9@gmail.com>
2025-11-30 13:24:31 +0530

    updated tf folders

--- root/modules/aws_network/aws_vpc.tf
+++ root/modules/aws_network/aws_vpc.tf
@@ -142,33 +142,3 @@ resource "aws_route_table_association" "private" {
     route_table_id = aws_route_table.private[count.index].id
 }
 
-variable "cidr_block" {
-    description = "CIDR block for the VPC"
-    type        = string
-}
-
-variable "public_subnet_cidrs" {
-    description = "CIDR blocks for public subnets"
-    type        = list(string)
-}
-
-variable "private_subnet_cidrs" {
-    description = "CIDR blocks for private subnets"
-    type        = list(string)
-}
-
-variable "availability_zones" {
-    description = "Availability zones for subnets"
-    type        = list(string)
-}
-
-variable "environment" {
-    description = "Environment name"
-    type        = string
-}
-
-variable "tags" {
-    description = "Common tags to apply to all resources"
-    type        = map(string)
-    default     = {}
-}
